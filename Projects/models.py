dcc1fb0693a2c6665ae304b58a4e57128ca0b1d5
RBryan05 <rg0358032023@unab.edu.sv>
2025-03-31 15:59:29 -0600

    Se implementó la posibilidad de saber el nombre del nivel en el cual se registrará el record

--- Projects/models.py
+++ Projects/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Jugadores(models.Model):
     Nombre = models.CharField(max_length=15, null=False, blank=False)
     Puntaje = models.CharField(max_length=10, null=False, blank=False)
+    ModoDeJuego = models.CharField(max_length=30, null=False, blank=False)
--- Projects/serializers.py
+++ Projects/serializers.py
@@ -4,4 +4,4 @@ from .models import Jugadores
 class JugadoresSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jugadores
-        fields = ('id', 'Nombre', 'Puntaje')
+        fields = ('id', 'Nombre', 'Puntaje', 'ModoDeJuego')
